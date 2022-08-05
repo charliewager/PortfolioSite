6289f8938850377a7740657975dae2648df4b616
Charlie Wager <charliewager@uvic.ca>
2022-08-05 13:14:45 -0700

    resized two images and made the image container as well as chose and formatted images for the album picker project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,65 @@
 import Card from "../components/Card";
+import React from "react";
 
 function Projects() {
 
+    const [winWidth, setWinWidth] = React.useState(window.innerWidth)
+    React.useEffect(() => {
+
+        const handler = () => {
+            setWinWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', handler)
+
+        return () => {window.removeEventListener('resize', handler)}
+
+    })
+
+    let descriptionStyle;
+    let textStyle;
+    let pageStyle;
+
+    if(winWidth >= 600){
+
+        descriptionStyle = {
+            margin: 0,
+            position: 'absolute',
+            width: '50%',
+            right: 0,
+            marginTop: '1.5rem',
+            marginBottom: '1.5rem'
+        };
+
+        textStyle = {
+            position: 'fixed',
+            width: '50%',
+            left: 0,
+            margin: 0,
+            top: '50%',
+            MsTransform: 'translateY(-50%)',
+            transform: 'translateY(-50%)'
+        };
+
+        pageStyle = {};
+
+    }else{
+
+        descriptionStyle = {};
+
+        textStyle = {};
+
+        pageStyle = {
+            display: 'flex',
+            flexDirection: 'column',
+
+        }
+    }
+
+
     return(
-        <div className='ProjectPage'>
-            <div className='ProjectTextContainer'>
+        <div className='ProjectPage' style = {pageStyle}>
+            <div className='ProjectTextContainer' style = {textStyle}>
                 <h1 className='firstHeading'>
                     HERE ARE SOME PROJECTS I'VE DONE
                 </h1>
@@ -17,7 +72,7 @@ function Projects() {
                 </p>
 
             </div>
-            <div className='ProjectDescriptionContainer'>
+            <div className='ProjectDescriptionContainer' style = {descriptionStyle}>
 
                 <div className='AlbumPickerDescrip'>
                     <h1 className='firstHeading'>ALBUM PICKER</h1>
@@ -32,6 +87,11 @@ function Projects() {
                         decide what to make for dinner and much more. This project is less of an Album Picker and more of a shuffle
                         button for lists.
                     </p>
+                    <div className = 'imgContainer'>
+                        <img className = 'AlbumPickerImg' src= {process.env.PUBLIC_URL + '/AlbumPickerMain.jpg'} alt = 'Main Page'/>
+                        <img className = 'AlbumPickerImg' src= {process.env.PUBLIC_URL + '/AlbumPickerSelection.jpg'} alt = 'Selection Screen'/>
+                        <img className = 'AlbumPickerImg' src= {process.env.PUBLIC_URL + '/AlbumPickerView.jpg'} alt = 'View of the list'/>
+                    </div>
                 </div>
 
                 <div className='ProcessCalDescrip'>
